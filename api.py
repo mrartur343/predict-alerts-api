a0ca65016697205cd9f1cef3822772cadc21ab5a
mrartur343 <arturminesk@gmail.com>
2024-08-15 23:48:39 +0300

    API

--- api.py
+++ api.py
@@ -10,5 +10,4 @@ app = FastAPI()
 @app.get("/")
 def get_alerts_prediction(region_name: str):
 	predict_results = predict.predict_function(region_name+".pkl", datetime.datetime.now().strftime("%m/%d/%Y"), alert_threshold=0).to_dict('records')
-	print(predict_results)
 	return list(predict_results)
