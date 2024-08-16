ca5bb8ad1a69fc142db9f2d5809ead2b9aa40492
mrartur343 <arturminesk@gmail.com>
2024-08-16 21:47:31 +0300

    Days select

--- api.py
+++ api.py
@@ -8,6 +8,6 @@ import predict
 app = FastAPI()
 
 @app.get("/")
-def get_alerts_prediction(region_name: str,days: int):
-	predict_results = predict.predict_function(region_name+".pkl", datetime.datetime.now().strftime("%m/%d/%Y"), alert_threshold=0,days_to_predict=days).to_dict('records')
+def get_alerts_prediction(region_name: str,days: str):
+	predict_results = predict.predict_function(region_name+".pkl", datetime.datetime.now().strftime("%m/%d/%Y"), alert_threshold=0,days_to_predict=int(days)).to_dict('records')
 	return list(predict_results)
